910aad5f4fb12f04de9fe97decd9c94ad9710eab
Владимир Сидоров <lorfol2802@outlook.com>
2017-12-23 17:20:35 +0200

    Add project files.

---
+++ Modul 11 Black Jack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_11_Black_Jack
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
